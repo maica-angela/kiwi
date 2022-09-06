223af99a5065df31fb550b7a3856b024c63a7928
maica-angela <accia.alena21@gmail.com>
2022-09-06 17:26:56 +0800

    updated base url

--- tcms/issuetracker/tests/test_jira.py
+++ tcms/issuetracker/tests/test_jira.py
@@ -45,7 +45,7 @@ class TestJIRAIntegration(APITestCase):
         bug_system = BugSystem.objects.create(  # nosec:B106:hardcoded_password_funcarg
             name="JIRA at kiwitcms.atlassian.net",
             tracker_type="tcms.issuetracker.types.JIRA",
-            base_url="https://jmeter.atlassian.net/",
+            base_url="https://jmeter.atlassian.net/browse/TBL-1",
             api_username=os.getenv("JIRA_BUGTRACKER_INTEGRATION_API_USERNAME"),
             api_password=os.getenv("JIRA_BUGTRACKER_INTEGRATION_API_TOKEN"),
         )
